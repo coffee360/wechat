3ff1bf5d86321b357d2b17ac6af98364843e589a
coffee <164589895@qq.com>
2021-10-22 14:58:32 +0800

    1

---
+++ src/Open.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Phpfunction\Wechat;
+
+use Phpfunction\App\HttpApp;
+
+/**
+ * 微信开放平台
+ * Class Open
+ * @package Phpfunction\Wechat
+ */
+class Open
+{
+    public $app_id  = "";
+    public $secret  = "";
+    public $token   = "";
+    public $aes_key = "";
+
+
+    public function getAccessToken($code)
+    {
+        $url = "https://api.weixin.qq.com/sns/oauth2/access_token?";
+        $url .= "appid=" . $this->app_id . "&secret=" . $this->secret;
+        $url .= "&code=" . $code . "&grant_type=authorization_code";
+
+        return (new HttpApp())->get($url);
+    }
+
+}
