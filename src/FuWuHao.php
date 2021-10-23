df183b2f7dfe5b4f28d5ec1bf2a1140a90c61636
coffee <164589895@qq.com>
2021-10-23 14:05:32 +0800

    1

--- src/FuWuHao.php
+++ src/FuWuHao.php
@@ -2,6 +2,8 @@
 
 namespace Phpfunction\Wechat;
 
+use Phpfunction\App\HttpApp;
+
 /**
  * 微信公众号——服务号
  * Class Open
@@ -9,6 +11,23 @@ namespace Phpfunction\Wechat;
  */
 class FuWuHao
 {
-   
+
+    public $app_id = "";
+    public $secret = "";
+
+
+    /**
+     * 由code获得access_token
+     * @param $code
+     * @return bool|string
+     */
+    public function getAccessToken($code)
+    {
+        $url = "https://api.weixin.qq.com/sns/oauth2/access_token?";
+        $url .= "appid=" . $this->app_id . "&secret=" . $this->secret;
+        $url .= "&code=" . $code . "&grant_type=authorization_code";
+
+        return (new HttpApp())->get($url);
+    }
 
 }
